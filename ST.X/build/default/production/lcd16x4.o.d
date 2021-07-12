4b6906d11cdd4116b67b94aebf386c610eeb94a7
Matx26 <lmatias2611@gmail.com>
2021-07-12 18:26:04 -0300

    Se actulliza el archivo .ignore, se borran de este las carpetas de Mplab

---
+++ ST.X/build/default/production/lcd16x4.o.d
@@ -0,0 +1,3 @@
+build/default/production/lcd16x4.o: lcd16x4.c lcd16x4.h
+
+lcd16x4.h:
---
+++ ST.X/build/default/production/mainV211.o.d
@@ -0,0 +1,5 @@
+build/default/production/mainV211.o: mainV211.c ConfigBits.h lcd16x4.h
+
+ConfigBits.h:
+
+lcd16x4.h:
